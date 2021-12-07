09c47bcc84ab1d37248365874cc1edc31a579cf2
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2021-12-08 02:21:04 +0530

    Updat e

---
+++ CF/Round #110/A_Fair_Playoff.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define SZ(x) ((int)(x).size())
+typedef vector<int> VI;
+typedef long long ll;
+typedef pair<int,int> PII;
+typedef double db;
+mt19937 mrand(random_device{}()); 
+const ll mod=1000000007;
+int rnd(int x) { return mrand() % x;}
+ll powmod(ll a,ll b) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ll gcd(ll a,ll b) { return b?gcd(b,a%b):a;}
+
+const int N=201000;
+int n,m,_;
+
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        VI v(4, 0), cpy(4, 0);
+        cin >> v[0] >> v[1] >> v[2] >> v[3];
+
+        cpy = v;
+
+        sort(all(cpy));
+
+        bool done1=false, done2=false, done3=false, done4=false;
+
+        rep (i, 0, 2) {
+            if (v[i] == cpy[2]) done1 = true;
+            else if (v[i] == cpy[3]) done2 = true;
+        }
+
+
+        rep (i, 2, 4) {
+            if (v[i] == cpy[2]) done3 = true;
+            else if (v[i] == cpy[3]) done4 = true;
+        }
+
+        if ((done1 && done3) || (done1 && done4) || (done2 && done3) || (done2 && done4)) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+
+    return 0;
+}
---
+++ CF/Round #110/B_Array_Reodering.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define SZ(x) ((int)(x).size())
+typedef vector<int> VI;
+typedef long long ll;
+typedef pair<int,int> PII;
+typedef double db;
+mt19937 mrand(random_device{}()); 
+const ll mod=1000000007;
+int rnd(int x) { return mrand() % x;}
+ll powmod(ll a,ll b) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ll gcd(ll a,ll b) { return b?gcd(b,a%b):a;}
+
+const int N=201000;
+int n,m,_;
+
+
+
+
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, count=0;
+        cin >> n;
+
+        VI a(n, 0);
+
+        rep (i, 0, n) cin >> a[i];
+
+
+        sort(all(a));
+        reverse(all(a));
+
+        rep (i, 0, n) {
+            rep (j, i+1, n) {
+                if (gcd(a[i], 2*a[j]) > 1) count++;
+            }
+        }
+
+        cout << count << endl;
+    }
+
+    return 0;
+}

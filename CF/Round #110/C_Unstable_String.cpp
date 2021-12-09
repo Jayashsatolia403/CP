4c78f64d6ec14e76df45afd6cd065e84936f6423
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2021-12-09 17:29:50 +0530

    Added D

--- CF/Round #110/C_Unstable_String.cpp
+++ CF/Round #110/C_Unstable_String.cpp
@@ -22,31 +22,6 @@ const int N=201000;
 int n,m,_;
 
 
-
-int is_stable(vector<vector<int>> dp, string s, int i, int j, char prev) {
-    if (i > j) return 1;
-
-    if (i==j) {
-        if (s[i] != prev) return 1;
-        if (s[i] == prev) return -1;
-    }
-
-    if ((j-i)%2!=0) {
-        if (s[i] == s[j] && s[i] != '?' && s[j] != '?') return -1;
-    }
-    else {
-        if (s[i] != s[j] && s[i] != '?' && s[j] != '?') return -1;
-    }
-
-    if (dp[i][j] != 0) return dp[i][j];
-
-    dp[i][j] = is_stable(dp, s, i+1, j-1, s[i]);
-
-    return dp[i][j];
-}
-
-
-
 int main() {
     int t;
     cin >> t;
@@ -55,18 +30,21 @@ int main() {
         string s;
         cin >> s;
 
-        int n = s.length(), count=0;
+        int n = s.length(), ans=0, x[2] = {-1, -1};
 
         rep (i, 0, n) {
-            rep (j, i+1, n) {
-                if (is_stable(vector<VI>(n, VI(n, 0)), s, i, j, s[i]) == 1) {
-                    count++;
-                }
+            int c = s[i] - '0';
+
+            if (c==0 || c==1) {
+                x[c^(i%2)] = i;
             }
+
+            int mn = min(x[0], x[1]);
+
+            ans += i-mn;
         }
 
-
-        cout << count+s.length() << endl;
+        cout << ans << endl;
     }
 
     return 0;
